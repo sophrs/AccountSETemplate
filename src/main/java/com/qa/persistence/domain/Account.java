4c203626e019de983ee329426367bc62b4ed4f58
Admin <Admin@STUDENT05>
2019-01-31 09:44:14 +0000

    jave SE to java EE

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,16 +1,25 @@
 package com.qa.persistence.domain;
 
 import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
+@Entity
 public class Account {
 	
 	 private String firstName;
 	 private String lastName;
+	 
+	 @Size(min = 6, max = 6)
 	 private int accountNumber;
 	 
-
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+	 @Id
 	 private Long id;
-	 
+
 	 
 	
 	public Account(String firstName, String lastName, int accountNumber, Long id) {
@@ -30,6 +39,7 @@ public class Account {
 
 	public String getLastName() {
 		return lastName;
+		
 	}
 
 	public void setLastName(String lastName) {
